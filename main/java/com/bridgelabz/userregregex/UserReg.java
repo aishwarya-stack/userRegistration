7ac72bf689b69afbb99e296cd850c2081b233e1d
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-10-25 22:31:55 +0530

    Add:Validation LastName

--- main/java/com/bridgelabz/userregregex/UserReg.java
+++ main/java/com/bridgelabz/userregregex/UserReg.java
@@ -6,15 +6,23 @@ import java.util.regex.Pattern;
 public class UserReg {
 	public static final Scanner sc = new Scanner(System.in);
 	public final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+	public final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
 
 	// This method checks if the entered first name is valid
 	public boolean validateFirstName(String fname) {
 		Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
 		return pattern.matcher(fname).matches();
 	}
+	
+	public boolean validateLastName(String lname) {
+		Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
+		return pattern.matcher(lname).matches();
+	}
 
+	
 	// This is the main function
 	public static void main(String[] args) {
+		
 		System.out.println("Enter First Name: ");
 		String fname = sc.next();
 		UserReg obj = new UserReg();
@@ -23,5 +31,14 @@ public class UserReg {
 		} else {
 			System.out.println("First Name Invalid");
 		}
+		
+		System.out.println("\nEnter Last Name: ");
+		String lname = sc.next();
+		if (obj.validateFirstName(lname)) {
+			System.out.println("Last Name Valid");
+		} else {
+			System.out.println("Last Name Invalid");
+		}
 	}
-}
+
+		}}}
