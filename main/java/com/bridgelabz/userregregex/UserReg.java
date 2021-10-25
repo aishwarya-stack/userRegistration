38255b2950dfc726cadcc7008d87be272ed9129d
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-10-25 22:36:28 +0530

    Add:Validation Email Address

--- main/java/com/bridgelabz/userregregex/UserReg.java
+++ main/java/com/bridgelabz/userregregex/UserReg.java
@@ -7,22 +7,29 @@ public class UserReg {
 	public static final Scanner sc = new Scanner(System.in);
 	public final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
 	public final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+	public final String EMAIL_PATTERN = "[a-z0-9_]*[.a-z0-9_]*?@[a-z]*.[a-z]*[.a-z]?$";
 
 	// This method checks if the entered first name is valid
 	public boolean validateFirstName(String fname) {
 		Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
 		return pattern.matcher(fname).matches();
 	}
-	
+
+	// This method checks if the entered lawst name is valid
 	public boolean validateLastName(String lname) {
 		Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
 		return pattern.matcher(lname).matches();
 	}
 
-	
+	// This method checks if the entered email is valid
+	public boolean validateEmail(String email) {
+		Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+		return pattern.matcher(email).matches();
+	}
+
 	// This is the main function
 	public static void main(String[] args) {
-		
+
 		System.out.println("Enter First Name: ");
 		String fname = sc.next();
 		UserReg obj = new UserReg();
@@ -31,7 +38,7 @@ public class UserReg {
 		} else {
 			System.out.println("First Name Invalid");
 		}
-		
+
 		System.out.println("\nEnter Last Name: ");
 		String lname = sc.next();
 		if (obj.validateFirstName(lname)) {
@@ -39,6 +46,14 @@ public class UserReg {
 		} else {
 			System.out.println("Last Name Invalid");
 		}
-	}
 
-		}}}
+		System.out.println("\nEnter Email: ");
+		sc.nextLine();
+		String email = sc.next();
+		if (obj.validateEmail(email)) {
+			System.out.println("Email Valid");
+		} else {
+			System.out.println("Email Invalid");
+		}
+	}
+}
