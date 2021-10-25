b720bc447e00eaace9780420873b6d20102d1418
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-10-25 23:28:54 +0530

    Add:validate user with exception

--- test/java/com/bridgelabz/userregregex/UserRegParamertisedTest.java
+++ test/java/com/bridgelabz/userregregex/UserRegParamertisedTest.java
@@ -11,8 +11,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 @RunWith(Parameterized.class)
-
 public class UserRegParamertisedTest {
+
 	String input;
 	boolean expected;
 	UserReg object;
@@ -22,26 +22,28 @@ public class UserRegParamertisedTest {
 		object = new UserReg();
 	}
 
-	//Constructor
-	public void UserRegistrationParameterisedTest(String input, boolean expected) {
-		
+	// Constructor
+	public UserRegParamertisedTest(String input, boolean expected) {
+		super();
 		this.input = input;
 		this.expected = expected;
 
 	}
 
-
-
-	@Parameterized.Parameters	
+	@Parameterized.Parameters
 	public static Collection<Object[]> input() {
 
-		return Arrays.asList(new Object[][] { {"abc@yahoo.com",true }, { "abc",true }, { "abc-100@yahoo.com",true }, {"abc@.com.my",true}, });
+		return Arrays.asList(new Object[][] { { "abc@yahoo.com", true }, { "abc", true }, { "abc-100@yahoo.com", true },
+				{ "abc@.com.my", true }, });
 	}
-
 
 	@Test
 	public void emailTest() {
-		assertEquals(expected, object.validateEmail(input));
+		try {
+			assertEquals(expected, object.validateEmail(input));
+		} catch (UserRegistrationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
-
